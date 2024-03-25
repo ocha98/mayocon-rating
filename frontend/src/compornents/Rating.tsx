bb1cf86fba1d8885ade0dcb90b0a0278ec6f9b88
shinnshinn <48848793+ocha98@users.noreply.github.com>
2024-03-25 20:24:11 +0900

    [Fix] colors

--- frontend/src/compornents/Rating.tsx
+++ frontend/src/compornents/Rating.tsx
@@ -2,7 +2,7 @@
 const Colors = [
   "#5f5f5f", // 灰
   "#734e30", // 茶
-  "#ffcc00", // 緑
+  "#04aa4b", // 緑
   "#00d0f0", // 水
   "#1102d7", // 青
   "#ecec53", // 黄
@@ -13,7 +13,6 @@ const Colors = [
 const Rating = ({ rate, txt }: { rate: number, txt: string }) => {
   const idx = Math.floor(rate / 400);
   const color = Colors[Math.min(8, idx)];
-
   return (
     <span style={ { color: color}}>
         {txt}
